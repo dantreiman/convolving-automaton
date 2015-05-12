ad06acc7ffe88c6c39dcfd84a1013d93f68f0a2c
Dan Treiman <dan.treiman@gmail.com>
2015-05-12 01:57:29 -0700

    Render disk kernel.

--- Simulation.cc
+++ Simulation.cc
@@ -15,12 +15,20 @@ void Simulation::Init() {
 	CHECK_GL_ERROR("glGenTextures");
 	Buffer2D<float> inner_kernel(world_size_);
 	float inner_sum = CircularKernel(&inner_kernel, 20, 1);
-	glBindTexture (GL_TEXTURE_2D, inner_kernel_tex_);
+	glBindTexture(GL_TEXTURE_2D, inner_kernel_tex_);
+	
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	
 	CHECK_GL_ERROR("glBindTexture");
 	// glTexSubImage2D (GL_TEXTURE_2D, 0, 0, 0, world_size_.w, world_size_.h, GL_RED, GL_FLOAT, inner_kernel.data());
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA,
+	//if (dims==2) glTexImage2D (GL_TEXTURE_2D, 0, GL_R32F, NX,NY, 0, GL_RED, GL_FLOAT, NULL);
+	
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE,
 	             world_size_.w, world_size_.h,
-				0, GL_ALPHA, GL_FLOAT, inner_kernel.data());
+				0, GL_RED, GL_FLOAT, inner_kernel.data());
 	CHECK_GL_ERROR("glTexImage2D");
 }
 
